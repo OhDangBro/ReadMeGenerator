589a84efcbfb78738636a3b3dede78c3f765d64e
Will <103394466+OhDangBro@users.noreply.github.com>
2022-07-04 19:58:09 -0400

    added arguements

--- index.js
+++ index.js
@@ -2,6 +2,34 @@ const inquirer = require('inquirer');
 console.log("readme working");
 const fs = require('fs');
 const path = require('path');
+const heading = profileDataArgs[0];
+const title = profileDataArgs[1];
+const description = profileDataArgs[3];
+const table = profileDataArgs[4];
+const installation = profileDataArgs[5];
+const usage = profileDataArgs[6];
+const License = profileDataArgs[7];
+const contributing = profileDataArgs[8];
+const tests = profileDataArgs[9];
+const questions = profileDataArgs[10];
+const generatePage = (heading, title, ) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
 
 inquirer
     .prompt([
@@ -12,60 +40,64 @@ inquirer
         
         },
         {
-            type: 'title',
+            type: 'input',
             message: 'Title Name',
             name: 'title',
         
         },
         {
-            type: 'description',
+            type: 'input',
             message: 'Description of Project',
             name: 'description',
         
         },
         {
-            type: 'table-of-contents',
+            type: 'input',
             message: 'Table of contents',
-            name: 'table-of-contents',
+            name: 'table',
         
         },
         {
-            type: 'installation',
+            type: 'input',
             message: 'installation required',
             name: 'installation',
         
         },
         {
-            type: 'usage',
+            type: 'input',
             message: 'Used for',
             name: 'usage',
         
         },
         {
-            type: 'License',
+            type: 'input',
             message: 'License',
             name: 'License',
         
         },
         {
-            type: 'Contributing',
+            type: 'input',
             message: 'Contributors',
             name: 'contributing',
         
         },
         {
-            type: 'Tests',
+            type: 'input',
             message: 'Tests',
             name: 'tests',
         
         },
         {
-            type: 'Questions',
+            type: 'input',
             message: 'Questions',
             name: 'questions',
         
         },
     ])
+    .then((data) =>{
+        fs.writeFile('README.md', inputInfo(data), (err) => err ? console.log(err) : co
+        );
+    })
 
 
 
